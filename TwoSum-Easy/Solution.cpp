e89bea176cca5b339487b7a19ba02fff9e2bcb73
Sphesihle Madonsela <sphesihlemanuel99@gmail.com>
2023-03-04 18:08:20 +0200

    Imports added

--- TwoSum-Easy/Solution.cpp
+++ TwoSum-Easy/Solution.cpp
@@ -1,18 +1,19 @@
 #include<vector>
 #include<unordered_map>
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
         std::unordered_map<int, int> map;
 
-        for (vector<int>::iterator it = nums.begin(); it != nums.end(); it++) {
+        for (std::vector<int>::iterator it = nums.begin(); it != nums.end(); it++) {
             int complement = target - *it;
             if (map.find(complement) != map.end()) {
-                return { map[complement],static_cast<int>(distance(nums.begin(),it))};
+                return { map[complement],static_cast<int>(std::distance(nums.begin(),it))};
             }
-            map[*it] = static_cast<int>(distance(nums.begin(),it));
+            map[*it] = static_cast<int>(std::distance(nums.begin(),it));
        
         }
-        throw invalid_argument("No two sum solution");
+        throw std::invalid_argument("No two sum solution");
     }
 };
--- TwoSum-Easy/Solution.java
+++ TwoSum-Easy/Solution.java
@@ -1,4 +1,4 @@
-import java.util.*
+import java.util.*;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
